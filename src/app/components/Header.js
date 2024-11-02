cf38cb16a07b6d0e845bf346a95ee11a4e30512e
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-02 01:10:12 -0700

    user display

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,8 +4,13 @@ import Link from "next/link";
 export default function Header(){
   const session = useSession();
   console.log(session);
-  const status = session.status;
-
+  const status = session?.status;
+  const userData = session.data?.user;
+  let userName= userData?.name || userData?.email;
+  if(userName && userName.includes(' ')) {
+    userName = userName.split(' ')[0];
+  }
+  
 return (
     <header className="flex items-center justify-between">
       <Link className="text-black-600 gap-8\ text-2xl"
@@ -21,13 +26,17 @@ return (
         </nav>
 
         <nav className="flex items-center gap-4 text-gray-500 
-        font-semibold">
+        ">
           {status === 'authenticated' && (
-            <button
+            <>
+            <Link href={'/profile'} className="whitespace-nowrap">Hello,{userName}</Link>
+              <button
             onClick={() => signOut()}
             className="bg-green-600 rounded-full text-white px-8 py-2">
             Logout
             </button>
+            </>
+          
           )}
 
           {status === 'unauthenticated' && (
