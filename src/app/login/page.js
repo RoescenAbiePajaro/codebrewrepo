cfba0d4f21f25b881ba64fa77d5029e05cff4145
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-02 00:24:17 -0700

    authgoods

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -12,7 +12,7 @@ export default function LoginPage() {
         ev.preventDefault();
         setLoginInProgress(true);
 
-        await signIn('credentials', {email,password});
+        await signIn('credentials', {email,password,callbackUrl:'/'});
         
         setLoginInProgress(false);
     }
@@ -42,7 +42,7 @@ export default function LoginPage() {
                 <div className="my-4 text-center text-gray-500">
                     or login with provider
                 </div>
-                <button className="flex gap-4 justify-center">
+                <button onClick = {() => signIn('google', {callbackUrl:'/'})} className="flex gap-4 justify-center">
                     <Image src={'/google-icon.png'} alt={''} width={24} height={24} />
                     Login with Google
                 </button>
