6a5311d65ad053adeb346becd77a7a8a2dcdc5cb
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-04 19:23:45 -0800

    g

--- src/models/User.js
+++ src/models/User.js
@@ -5,10 +5,7 @@ const UserSchema = new Schema({
     name: {type: String},
     email: {type: String,required: true, unique:true},
     password: {type:String},
-    image:{type: String},
-    admin: {type:Boolean, default: false},
-    phone: {type: String},
-    streetAddress: {type: String},
+    
     
 }, {timestamps:true});
 export const User = models?.User || model('User', 
