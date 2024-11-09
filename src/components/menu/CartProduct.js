da97239568385b641e1983375e574b5452460742
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-09 08:07:38 -0800

    cartproduct js

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -6,7 +6,7 @@ export default function CartProduct({ product, onRemove, index }) {
   return (
     <div className="flex items-center gap-4 border-b py-4">
       <div className="w-24">
-        <Image width={240} height={240} src={product.image}/>
+        <Image width={240} height={240} src={product.image} alt={"productssimage"}/>
       </div>
       <div className="grow">
         <h3 className="font-semibold">
