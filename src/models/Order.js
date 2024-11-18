65d704cd4c47aa1be2c513340d7ec76915f09bdb
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-18 04:22:39 -0800

    jkhkjh

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,16 +1,16 @@
-// // /models/Order.js
-// import { model, models, Schema } from "mongoose";
+// /models/Order.js
+import { model, models, Schema } from "mongoose";
 
-// const OrderSchema = new Schema({
-//   userEmail: String,
-//   phone: String,
-//   streetAddress: String,
-//   cartProducts: [{
-//     name: String,
-//     price: Number,
-//     quantity: Number
-//   }],
-//   paid: { type: Boolean, default: false },
-// }, { timestamps: true }); // Adds createdAt and updatedAt fields
+const OrderSchema = new Schema({
+  userEmail: String,
+  phone: String,
+  streetAddress: String,
+  cartProducts: [{
+    name: String,
+    price: Number,
+    quantity: Number
+  }],
+  paid: { type: Boolean, default: false },
+}, { timestamps: true }); // Adds createdAt and updatedAt fields
 
-// export const Order = models.Order || model('Order', OrderSchema);
+export const Order = models.Order || model('Order', OrderSchema);
