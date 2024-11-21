11c3fa0c017eae61ebdb8eb34a51a48e334b0d0c
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-21 06:02:31 -0800

    a

--- src/components/menu/AddToCartButton.js
+++ src/components/menu/AddToCartButton.js
@@ -5,24 +5,33 @@ export default function AddToCartButton({
 }) {
   const isDisabled = stock <= 0;
 
-  if (!hasSizesOrExtras) {
+  const handleAddToCart = () => {
+    if (!isDisabled) {
+      // Add the item to the cart
+      onClick(basePrice);
+    }
+  };
+
+  if (!hasSizesOrExtras || isDisabled) {
     return (
       <div className="flying-button-parent mt-4">
-        <FlyingButton
-          targetTop={'5%'}
-          targetLeft={'95%'}
-          src={image || null}>
-          <div onClick={isDisabled ? null : onClick} className={isDisabled ? 'opacity-50 cursor-not-allowed' : ''}>
-            Add to cart ₱{basePrice}
-          </div>
-        </FlyingButton>
+        { !isDisabled && (
+          <FlyingButton
+            targetTop={'5%'}
+            targetLeft={'95%'}
+            src={image || null}>
+            <div onClick={handleAddToCart} className={isDisabled ? 'opacity-50 cursor-not-allowed' : ''}>
+              Add to cart ₱{basePrice}
+            </div>
+          </FlyingButton>
+        )}
       </div>
     );
   }
   return (
     <button
       type="button"
-      onClick={isDisabled ? null : onClick}
+      onClick={handleAddToCart}
       className={`mt-4 ${isDisabled ? 'bg-gray-500' : 'bg-green-500'} text-white rounded-full px-8 py-2`}
       disabled={isDisabled}
     >
