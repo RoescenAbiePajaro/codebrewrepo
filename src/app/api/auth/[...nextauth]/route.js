c863db0b66e16a77a718aa7eb00d88a0a6707dce
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-10 04:56:58 -0800

    ag

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,6 @@ import { authOptions } from "@/libs/authOptions";
 import mongoose from "mongoose";
 import db from "@/libs/db";
 
-
 export async function GET(req, res) {
   const handler = NextAuth(authOptions);
   return handler(req, res); // pass the request and response to the NextAuth handler
