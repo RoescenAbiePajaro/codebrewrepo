40e190bff2032dabb95d65fcf1c125a306348deb
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-10 02:11:49 -0800

    h

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,9 +2,14 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/authOptions";
 import mongoose from "mongoose";
+import db from "@/libs/db";
 
-
-
+export async function connectDB() {
+  if (mongoose.connections[0].readyState) {
+    return; // If already connected
+  }
+  await mongoose.connect(process.env.MONGO_URL); // No need to pass deprecated options
+}
 
 export async function GET(req, res) {
   const handler = NextAuth(authOptions);
