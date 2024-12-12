1dc1f91ae94ad27525f53b7601d1d54afca0a7ad
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-12 07:11:37 -0800

    j

--- src/components/layout/InfoBox.js
+++
@@ -1,7 +0,0 @@
-export default function InfoBox({children}){
-    return(
-        <div className="text-center text-gray-500 bg-blue-300 p-4 rounded-lg border border-blue-300">
-            {children}</div>
-    );
-
-}
