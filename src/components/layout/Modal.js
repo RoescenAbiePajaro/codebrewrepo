18d7243101eaea492ef804d28765b8ff1b387432
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-13 04:18:03 -0800

    omki

--- src/components/layout/Modal.js
+++ src/components/layout/Modal.js
@@ -96,13 +96,13 @@ export default function Modal({ user, isOpen, onClose }) {
         </div>
         <div className="flex justify-end gap-4">
           <button
-            className="px-4 py-2 bg-gray-500 text-white rounded-md"
+            className="px-4 py-2 bg-red-500 text-white rounded-md"
             onClick={onClose}
           >
             Cancel
           </button>
           <button
-            className="px-4 py-2 bg-green-600 text-white rounded-md"
+            className="px-4 py-2 bg-green-500 text-white rounded-md"
             onClick={handleSave}
           >
             Save
